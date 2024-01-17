ea655f0e52188f972e0fe55b31b7d2a8da724429
Adzi Purnomo <adzypurnomo@gmail.com>
2024-01-17 15:43:30 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                <span class="blue bolder">Data Mining</span>
+                Apriori Toko Parfum &copy; 2023 | Distributed by <a href="https://elektronikaku-smk1.blogspot.com" target="_blank" rel="noopener noreferrer">Blog Adzi Purnomo</a>
+            </span>
+        </div>
+    </div>
+</div>
